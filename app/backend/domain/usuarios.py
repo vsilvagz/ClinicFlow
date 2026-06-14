84fcdf9da6fb711f2b92f50d60914a6952f5b7ed
Víctor Silva <victor.silva.g@ug.uchile.cl>
2026-06-13 21:57:00 -0400

    se añaden clases de usuarios

--- app/backend/domain/usuarios.py
+++ app/backend/domain/usuarios.py
@@ -1,1 +1,33 @@
 """Tipos de usuario del sistema y sus permisos."""
+
+
+class Usuario:
+    def __init__(self, RUN_usuario: int, nombre: str, correo: str, telefono: int):
+        self.RUN_usuario = RUN_usuario
+        self.nombre = nombre
+        self.correo = correo
+        self.telefono = telefono
+
+
+class Paciente(Usuario):
+    def __init__(self, RUN_usuario: int, nombre: str, correo: str, telefono: int):
+        super().__init__(RUN_usuario, nombre, correo, telefono)
+        self.derivaciones_especialidades_permitidas: list[str] = []
+
+
+class Medico(Usuario):
+    def __init__(self, RUN_usuario: int, nombre: str, correo: str, telefono: int, especialidad):
+        super().__init__(RUN_usuario, nombre, correo, telefono)
+        self.especialidad = especialidad
+
+
+class Recepcionista(Usuario):
+    def __init__(self, RUN_usuario: int, nombre: str, correo: str, telefono: int, clinica):
+        super().__init__(RUN_usuario, nombre, correo, telefono)
+        self.clinica = clinica
+
+
+class Administrador(Usuario):
+    def __init__(self, RUN_usuario: int, nombre: str, correo: str, telefono: int):
+        super().__init__(RUN_usuario, nombre, correo, telefono)
+        self.acceso_vip = True
